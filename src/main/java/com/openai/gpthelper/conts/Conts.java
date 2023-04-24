6ddacf110272c91d1212710155dee34415c83eae
wangchao <agentwangz@163.com>
2023-04-24 20:01:14 +0800

    初始化代码

--- src/main/java/com/openai/gpthelper/conts/Conts.java
+++ src/main/java/com/openai/gpthelper/conts/Conts.java
@@ -4,17 +4,17 @@ public class Conts {
 
     public static String[] rooms =new String[]{"38840239210@chatroom"};
     // Replace API_KEY with your OpenAI API key
-    public static String apiKey = "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";
+    public static String apiKey = "xxxx";
     // Replace proxyHost and proxyPort with your proxy server hostname and port number
 
-    public static String proxyHost = "154.195.50.245";
-    public static int proxyPort = 2000;
+    public static String proxyHost = "xxx";
+    public static int proxyPort = 111;
     // Replace proxyUsername and proxyPassword with your authentication credentials
 
-    public static String proxyUsername = "wangchao";
-    public static String proxyPassword = "wangchao";
+    public static String proxyUsername = "xxx";
+    public static String proxyPassword = "xxx";
 
-    public static String hwuURL = "http://60.204.148.210:5188/API/gpt/reply";
+    public static String hwuURL = "http://xxx:xxx/API/gpt/reply";
 
 
 }
